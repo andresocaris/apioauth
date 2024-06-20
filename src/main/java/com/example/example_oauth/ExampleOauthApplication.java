0844fa304440abc83805e73b71c0571951fb86d8
andresocaris <andresocaris@gmail.com>
2024-06-20 13:29:43 -0500

    primer commit

---
+++ src/main/java/com/example/example_oauth/ExampleOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/example_oauth/ExampleOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
